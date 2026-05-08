254de4961caeaeb4b6238f80a22da11827755bde
Nusvara <yusufusman11111111@gmail.com>
2026-05-08 09:49:49 +0700

    smth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,18 +18,21 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="{{ url('/') }}"><img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo" style="height: 3rem;"></a>
+                        <a href="{{ url('/') }}">
+                            <img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo">
+                        </a>
                     </div>
                     <h1 class="auth-title">Log in.</h1>
                     <p class="auth-subtitle mb-5">Masuk dengan data yang anda daftarkan.</p>
 
                     <form action="{{ route('login.attempt') }}" method="POST">
                         @csrf
-                        
-                        @if($errors->any())
+
+                        {{-- Alert Error --}}
+                        @if ($errors->any())
                             <div class="alert alert-danger">
                                 <ul class="mb-0">
-                                    @foreach($errors->all() as $error)
+                                    @foreach ($errors->all() as $error)
                                         <li>{{ $error }}</li>
                                     @endforeach
                                 </ul>
@@ -36,30 +40,45 @@
                         @endif
 
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" name="username" class="form-control form-control-xl" placeholder="Username" value="{{ old('username') }}" required autofocus>
+                            <input type="text" name="username" class="form-control form-control-lg"
+                                placeholder="Username" value="{{ old('username') }}" required autofocus>
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
                         </div>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" name="password" class="form-control form-control-xl" placeholder="Password" required>
+                            <input type="password" name="password" class="form-control form-control-lg"
+                                placeholder="Password" required>
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
                         </div>
-                        <div class="form-check mb-4">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember">
-                            <label class="form-check-label" for="remember">Ingat saya</label>
+
+                        <div class="form-check form-check-lg d-flex align-items-end">
+                            <input class="form-check-input me-2" type="checkbox" name="remember" id="remember">
+                            <label class="form-check-label text-gray-600" for="remember">
+                                Ingat saya
+                            </label>
                         </div>
+
                         <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
                     </form>
+
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <p><a class="font-bold" href="#">Lupa password?</a>.</p>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-7 d-none d-lg-block">
-                <div id="auth-right">
+                <div id="auth-right" class="shadow-lg" 
+                     style="background: url('{{ asset('assets/images/bg/thumbnail.png') }}'), #435ebe; 
+                            background-size: cover; 
+                            background-position: center; 
+                            box-shadow: inset 10px 0 100px -10px rgba(0,0,0,0.5) !important;">
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
